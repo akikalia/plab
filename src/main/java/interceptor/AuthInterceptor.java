cb0a57ec87a3599eb2312095711e5e87218d4795
akikalia <akika16@freeuni.edu.ge>
2020-07-31 22:23:40 +0400

    AuthInterceptor working (when not logged in, can't access insider data);

--- src/main/java/interceptor/AuthInterceptor.java
+++ src/main/java/interceptor/AuthInterceptor.java
@@ -1,16 +1,23 @@
 package interceptor;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Component
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getRequestURI().equals("/") || request.getRequestURI().startsWith("/resources/") ||
+                request.getRequestURI().startsWith("/login") || request.getRequestURI().startsWith("/register")
+                || request.getRequestURI().startsWith("/register")){
+            return true;
+        }
         if (request.getSession() == null){
             response.sendRedirect("/");
             return false;
@@ -23,4 +30,13 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
         return true;
     }
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                Exception exception) {
+    }
 }
